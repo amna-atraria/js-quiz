8642d6a2488463c72b1a437776fb2cfab23d1c97
amna-attaria46 <amnaataria48@gmail.com>
2024-08-15 08:39:18 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+
+function result() {
+
+    var score = 0;
+    
+    if (document.getElementById('correct 1').checked) {
+        score++;
+    }
+
+    if (document.getElementById('correct 2').checked) {
+        score++;
+    }
+
+    if (document.getElementById('correct 3').checked) {
+        score++;
+    }
+
+    if (document.getElementById('correct 4').checked) {
+        score++;
+    }
+    if (document.getElementById('correct 5').checked) {
+        score++;
+    }
+    if (document.getElementById('correct 6').checked) {
+        score++;
+    }
+    if (document.getElementById('correct 7').checked) {
+        score++;
+    }
+    if (document.getElementById('correct 8').checked) {
+        score++;
+    }
+    if (document.getElementById('correct 9').checked) {
+        score++;
+    }
+    if (document.getElementById('correct 10').checked) {
+        score++;
+    }
+    
+    alert("Your score is: " + score);
+     
+}
+
+
+   
+
+
+   
